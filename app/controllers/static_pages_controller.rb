e626be2fbc0ebc47f72c6cb08f09a1b6e3757b93
Aaron Contreras <aaronlcaq@gmail.com>
2021-01-31 20:29:28 -0500

    Add popular feed when no flickr id searched for

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,23 @@
 # Static Pages resource
 class StaticPagesController < ApplicationController
   def home
-    if params[:user_id]
+    flickr = Flickr.new
+
+    if flickr_id_searched?
       begin
-        flickr = Flickr.new
         @user_id = params[:user_id]
         @photos = flickr.photos.search(user_id: params[:user_id])
-      rescue Flickr::FailedResponse => e
+      rescue Flickr::FailedResponse
         flash[:alert] = "Flickr user id #{@user_id} doesn't exist."
       end
+    else
+      @photos = flickr.interestingness.getList['photos']
     end
+  end
+
+  private
+
+  def flickr_id_searched?
+    !!params[:user_id]
   end
 end
